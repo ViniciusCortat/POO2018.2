d0826756f3f4d21400372fc8e74145a60b19be19
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2018-11-23 16:06:14 -0200

    Update Main.java
    só troquei como o menu é iniciado

--- SuperLudo1620995-1312093/src/Ludo/Main.java
+++ SuperLudo1620995-1312093/src/Ludo/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Janela j = new Janela();
-		Menu m = new Menu();
+		Menu m = Menu.getInstance();
 		Tabuleiro t = new Tabuleiro();
 		j.add(m);
 		j.add(t);
